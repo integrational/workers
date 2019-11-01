6e04d3ac5a44a4af6979af382c2548b4891a05ff
GeraldLoeffler <gerald.loeffler@googlemail.com>
2019-11-01 15:23:48 +0100

    added channel directions

--- workers.go
+++ workers.go
@@ -26,7 +26,7 @@ func Do(todos []int, do func(todo int) (done int), numWorkers int) (dones []int)
 	return collectDones(doneChan)
 }
 
-func forkWorkers(todoChan chan int, do func(int) int, doneChan chan int, numWorkers int) *sync.WaitGroup {
+func forkWorkers(todoChan <-chan int, do func(int) int, doneChan chan<- int, numWorkers int) *sync.WaitGroup {
 	workers := &sync.WaitGroup{}
 	for n := 0; n < numWorkers; n++ {
 		workers.Add(1)
@@ -35,26 +35,26 @@ func forkWorkers(todoChan chan int, do func(int) int, doneChan chan int, numWork
 	return workers
 }
 
-func worker(todoChan chan int, do func(int) int, doneChan chan int, workers *sync.WaitGroup) {
+func worker(todoChan <-chan int, do func(int) int, doneChan chan<- int, workers *sync.WaitGroup) {
 	defer workers.Done()
 	for todo := range todoChan {
 		doneChan <- do(todo)
 	}
 }
 
-func submitTodos(todos []int, todoChan chan int) {
+func submitTodos(todos []int, todoChan chan<- int) {
 	for _, todo := range todos {
 		todoChan <- todo
 	}
 	close(todoChan)
 }
 
-func waitForWorkers(workers *sync.WaitGroup, doneChan chan int) {
+func waitForWorkers(workers *sync.WaitGroup, doneChan chan<- int) {
 	workers.Wait()
 	close(doneChan)
 }
 
-func collectDones(doneChan chan int) []int {
+func collectDones(doneChan <-chan int) []int {
 	dones := make([]int, len(doneChan))
 	for done := range doneChan {
 		dones = append(dones, done)
