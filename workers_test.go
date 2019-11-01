6cd38d6a2a419af2b1f24f97d04fce8ae42e929d
GeraldLoeffler <gerald.loeffler@googlemail.com>
2019-11-01 17:50:10 +0100

    better tests

--- workers_test.go
+++ workers_test.go
@@ -1,15 +1,16 @@
 package workers
 
 import (
+	"math/rand"
 	"reflect"
 	"sort"
 	"testing"
 )
 
 func TestDo(t *testing.T) {
-	primes := []int{2, 3, 5, 7, 11, 13}
 	times1000 := func(todo int) int { return todo * 1000 }
-	primesTimes1000 := []int{2000, 3000, 5000, 7000, 11000, 13000} // sorted
+	few, few1000 := []int{2, 3, 5, 7, 11}, []int{2000, 3000, 5000, 7000, 11000} // sorted
+	many, many1000 := testDataForMany(times1000)                                // sorted
 
 	cases := []struct {
 		todos      []int
@@ -29,10 +30,15 @@ func TestDo(t *testing.T) {
 		{[]int{}, times1000, 0, []int{}},
 		{[]int{}, times1000, 1, []int{}},
 		{[]int{}, times1000, 10, []int{}},
-		{primes, times1000, -1, primesTimes1000},
-		{primes, times1000, 0, primesTimes1000},
-		{primes, times1000, 1, primesTimes1000},
-		{primes, times1000, 10, primesTimes1000},
+		{few, nil, -1, few},
+		{few, nil, 0, few},
+		{few, nil, 1, few},
+		{few, nil, 10, few},
+		{few, times1000, -1, few1000},
+		{few, times1000, 0, few1000},
+		{few, times1000, 1, few1000},
+		{few, times1000, 10, few1000},
+		{many, times1000, 10, many1000},
 	}
 	for _, c := range cases {
 		got := Do(c.todos, c.do, c.numWorkers)
@@ -40,4 +46,17 @@ func TestDo(t *testing.T) {
 			t.Errorf("DoWorkers(%v, %v) == %v, want %v", c.todos, c.numWorkers, got, c.want)
 		}
 	}
+}
+
+func testDataForMany(times1000 func(int) int) (many []int, manyTimes1000 []int) {
+	const howMany = 6789
+
+	many = make([]int, howMany)
+	manyTimes1000 = make([]int, howMany)
+	for i := 0; i < howMany; i++ {
+		many[i] = rand.Intn(howMany * 3)
+		manyTimes1000[i] = times1000(many[i])
+	}
+	sort.Ints(manyTimes1000)
+	return
 }
